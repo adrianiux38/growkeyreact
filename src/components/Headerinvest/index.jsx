a1aa5ae5c19d56123314fa0cb4b01b01a39e8f48
Sticky <adrian.gutierrez@techsoftconsulting.mx>
2022-12-07 10:29:01 -0800

    listo

--- src/components/Headerinvest/index.jsx
+++ src/components/Headerinvest/index.jsx
@@ -4,56 +4,68 @@ import Computer from './assets/computer.svg';
 import "./Headerinvest.css";
 
 export const Headerinvest = () => {
-  return (
-    <div id='headerapply'>
-        <div className="contenedor">
-            <div id="platformPreview">
-                <img src={Computer} alt="Growkey platform" />
-                <img id="store" src={Store} alt="Ecommerce store" />
-            </div>
-            <div id="form">
-                <h2>Register Now</h2>
-                <form>
-                    <div className='inputElement'>
-                        <label>
-                            Full Name
-                            <input type="text" name="Full name"/>
-                        </label>
-                    </div>
-                    <div className='inputElement'>
-                        <label>
-                             Email
-                            <input type="text" name="Email"/>
-                        </label>
-                    </div>
-                    <div className='inputElement'>
-                        <label>
-                            Phone
-                            <input type="text" name="Full name"/>
-                        </label>
-                    </div>
-                    <div className='inputElement'>
-                        <label>
-                            URL
-                            <input type="text" name="Full name"/>
-                        </label>
-                    </div>
-                    <div className='selectElement'>
-                        Select the ammount yout would like to receive
-                    <select>
-                        <option selected value="1,000 USD - 10,000 USD">1,000USD - 10,000 USD</option>
-                        <option value="10,000USD - 50,000 USD">10,000USD - 50,000 USD</option>
-                        <option selected value="50,000 USD - 100,000 USD">50,000 USD - 100,000 USD</option>
-                        <option value="+100,000 USD">+100,000 USD</option>
-                    </select>
-                    </div>
-                    <button id='enviaDatos'>
-                        Submit
-                    </button>
-                </form>
-            </div>
 
+    const handleChange = ({event}) => {
+        this.setState({[event.target.name]: event.target.value});
+        }
+     
+    const handleSubmit = ({event}) => {
+    
+    fetch('http://localhost:3000/store-data', {
+        method: 'POST',
+        // We convert the React state to JSON and send it as the POST body
+        body: JSON.stringify(this.state)
+        }).then(function(response) {
+        console.log(response)
+        return response.json();
+        });
+    
+    event.preventDefault();
+    }
+    
+    return (
+        <div id='headerinvest'>
+            <div className="contenedor">
+                <div id="platformPreview">
+                    <img src={Computer} alt="Growkey platform" />
+                    <img id="store" src={Store} alt="Ecommerce store" />
+                </div>
+                <div id="form">
+                    <h2>Register Now</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className='inputElement'>
+                            <label>
+                                Full Name
+                                <input type="text" name="name" onChange={handleChange}/>
+                            </label>
+                        </div>
+                        <div className='inputElement'>
+                            <label>
+                                Email
+                                <input type="text" vallue="email" name="Email"/>
+                            </label>
+                        </div>
+                        <div className='inputElement'>
+                            <label>
+                                Phone
+                                <input type="text" name="Full name"/>
+                            </label>
+                        </div>
+                        <div className='selectElement'>
+                            Select the ammount yout would like to receive
+                        <select>
+                            <option selected value="1,000 USD - 10,000 USD">1,000USD - 10,000 USD</option>
+                            <option value="10,000USD - 50,000 USD">10,000USD - 50,000 USD</option>
+                            <option selected value="50,000 USD - 100,000 USD">50,000 USD - 100,000 USD</option>
+                            <option value="+100,000 USD">+100,000 USD</option>
+                        </select>
+                        </div>
+                        <input type="submit" value="Submit" id='enviaDatos'/>
+                    </form>
+                </div>
+
+            </div>
         </div>
-    </div>
-  );
+    );
 }
+
